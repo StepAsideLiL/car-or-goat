c463de27a9b4bece87f8828357ea46dc2b1df51d
Rifat Khan <rifat6633@gmail.com>
2024-03-01 20:53:43 +0600

    fix: overflowing height and width

--- components/playing-field/confetti-rain.tsx
+++ components/playing-field/confetti-rain.tsx
@@ -1,7 +1,9 @@
 import Confetti from "react-confetti";
 
 const ConfettiRain = () => {
-  return <Confetti className="min-w-full min-h-full overflow-hidden" />;
+  return (
+    <Confetti className="container min-w-full min-h-full overflow-hidden" />
+  );
 };
 
 export default ConfettiRain;
